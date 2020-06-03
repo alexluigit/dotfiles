01b3e973b447ae1d0bee409a90886c96bc1418e4
Alex <hellosimon1103@hotmail.com>
2020-06-03 21:40:35 +0800

    Rename or move dir

--- scripts/Schedule.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-/Users/simon/Applications/ariaTracker;
-killall aria2c;
-kill $(ps aux | grep app.js\ -p\ 10001 | awk '{print $2}'); 
-/usr/local/aria2/bin/aria2c -D --conf-path=/Users/simon/.config/aria2/aria2.conf; 
-/usr/local/bin/node /Users/simon/Applications/UnblockNeteaseMusic/app.js -p 10001:10002
-
-
-

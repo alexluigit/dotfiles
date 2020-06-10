64575d13bd40cc8c991a4929a4b2394ad51cf96d
Alex <hellosimon1103@hotmail.com>
2020-06-10 11:39:38 +0800

    Change some aliases

--- zsh/.config/zsh/alias.zsh
+++ zsh/.config/zsh/alias.zsh
@@ -4,12 +4,11 @@ command -v nvim >/dev/null && alias vim="nvim" vimdiff="nvim -d"
 alias md="mkdir -p"
 alias rd="rmdir"
 alias yd="youtube-dl --write-sub --write-auto-sub -o '~/Desktop/%(title)s-%(id)s.%(ext)s'"
-alias ydl="youtube-dl --write-sub --write-auto-sub -o '~/Desktop/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s'"
+alias ydl="youtube-dl --yes-playlist --write-sub --write-auto-sub -o '~/Desktop/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s'"
 alias ls="ls --group-directories-first --color=auto"
 alias la="ls -Flha --group-directories-first --color=auto"
-alias t="tmux -f ~/.config/tmux/tmux.conf new -A -s MAIN \; split-window -h \; select-pane -L \; resize-pane -Z"
+alias t="tmux-automation"
 alias ka="killall"
 alias v='nvim -c "let g:tty='\''$(tty)'\''"'
-# alias ssh='TERM=xterm ssh'
 # Git
 

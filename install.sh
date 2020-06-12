f2e5d735aa587c938e7c2520a84efbfe806c1010
Alex <hellosimon1103@hotmail.com>
2020-06-12 09:26:23 +0800

    Add install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo $PWD
+ln -s $PWD/zshenv ~/.zshenv
+ln -s $PWD/zsh ~/.config/zsh
+ln -s $PWD/nvim ~/.config/nvim
+ln -s $PWD/karabiner ~/.config/karabiner
+ln -s $PWD/tmux ~/.config/tmux
+ln -s $PWD/tmux/tmux.conf ~/.tmux.conf
+ln -s $PWD/vifm ~/.config/vifm
+ln -s $PWD/shadowsocks ~/.ShadowsocksX-NG
+ln -s $PWD/scripts ~/bin/scripts
+ln -s $PWD/ranger ~/.config/ranger
+
+ln -s $PWD/ssh ~/.ssh
+ln -s $PWD/Rime ~/Library/Rime
+ln -s $PWD/git ~/.config/git

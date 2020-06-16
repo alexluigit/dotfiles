73d66b3fa53ede793af1511f6557c78bad861e74
Alex <hellosimon1103@hotmail.com>
2020-06-17 01:18:00 +0800

    Refactor fzf-widgets

---
+++ zsh/fzf-widgets.zsh
@@ -0,0 +1,50 @@
+# TODO fzf-open --> fzf-music & fzf-video
+# TODO fzf-vim -->  fzf-edit
+# TODO fzf-git-checkout
+fzf-open() { fd -t f -L --ignore-file /Volumes/HDD/.fdignore . /Volumes/HDD | fzf -m | gxargs -ro -d '\n' open >/dev/null }
+zle -N fzf-open
+
+fzf-vim() {
+  fd -t f -H -I --ignore-file ~/.fdignore . ~ | fzf -m | gxargs -ro -d '\n' nvim 2>&-
+  zle reset-prompt 2>&1; zle-line-init 2>&1
+}
+zle -N fzf-vim
+
+fzf-note() {
+  local NPath='/Users/simon/Documents/AllNotes/'
+  fd -t f . $NPath | sed "s|$NPath||" | fzf -m --preview="bat -p --color=always $NPath{}" | sed "s|^|$NPath|" | gxargs -ro -d '\n' nvim
+  zle reset-prompt 2>&1; zle-line-init 2>&1
+}
+zle -N fzf-note
+
+fzf-cd() {
+  local destination=$(fd -H -t d -d ${2:-5} . ${1:-/} | fzf --preview='tree -L 1 {}') 
+  [[ ! -z "$destination" ]] && cd "$destination" 
+  zle reset-prompt 2>&-
+}
+zle -N fzf-cd
+
+fzf-project() { 
+  local prefix="/Users/$USER/dev/"
+  local destination=$(fd -H -t d -d 1 . ~/dev | sed "s|$prefix||" | fzf --preview="tree -L 1 $prefix{}" )
+  [[ ! -z "$destination" ]] && cd "/Users/simon/dev/$destination" 
+  zle reset-prompt 2>&1; zle-line-init 2>&1
+}
+zle -N fzf-project
+
+fzf-history() {
+  local selected num
+  setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
+  selected=( $(fc -rl 1 |
+    FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" "fzf" ) )
+  local ret=$?
+  if [ -n "$selected" ]; then
+    num=$selected[1]
+    if [ -n "$num" ]; then
+      zle vi-fetch-history -n $num
+    fi
+  fi
+  zle reset-prompt
+  return $ret
+}
+zle -N fzf-history

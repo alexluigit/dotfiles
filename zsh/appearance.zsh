959e934a88c0e9567cbb941695351cacc988d567
Alex <hellosimon1103@hotmail.com>
2020-06-24 12:53:14 +0800

    Fix $SHLVL display in nvim terminal

--- zsh/appearance.zsh
+++ zsh/appearance.zsh
@@ -9,13 +9,13 @@ ZSH_HIGHLIGHT_STYLES[alias]=fg=yellow,bold
 ZSH_HIGHLIGHT_STYLES[builtin]=fg=blue,bold
 ZSH_HIGHLIGHT_STYLES[function]=fg=yellow,bold
 ZSH_HIGHLIGHT_STYLES[command]=fg=blue,bold
-# Handle shell level for no-tmux, default-tmux, tmux-automation sessions
-function shell_level() { 
-  local shlv
-  [[ -z "$TMUX" ]] &&  shlv=$SHLVL || \
-  { export ZLE_RPROMPT_INDENT=0; [[ -z $(ps aux | grep '[.]/.tmux') ]] && \
-  shlv=$(($SHLVL - 1)) || shlv=$(($SHLVL - 2)) };
-  [[ shlv -gt 1 ]] && echo "%F{yellow}%B[$shlv]%b%f " || echo ""
+# Handle shell level for tmux / tmux-automation / nvim terminal
+function shell_level() {
+  local shlv=$SHLVL
+  [[ -n "$TMUX" ]] && { shlv=$((shlv-1)); export ZLE_RPROMPT_INDENT=0 }
+  [[ -n $(ps aux | grep '[.]/.tmux') ]] && shlv=$((shlv-1))
+  [[ -n "$VIM" ]] && shlv=$((shlv-1))
+  [[ $shlv -gt 1 ]] && echo "%F{yellow}%B[$shlv]%b%f " || echo ""
 }
 # Check background_job, super_user, exit code (Use ternary operators here)
 local bg_jobs="%(1j.%{$fg_bold[magenta]%} .)"
@@ -23,7 +23,9 @@ local privileges="%(#.%{$fg_bold[red]%} .)"
 local ret_status="%(?:%{$fg_bold[cyan]%} :%{$fg_bold[red]%} %s)"
 PROMPT="$(shell_level)$bg_jobs$privileges$ret_status%{$reset_color%} "
 # Outputs current branch info in prompt format
-local staged unstaged untracked
+local staged=""
+local unstaged=""
+local untracked=""
 function git_prompt_info() {
   local ref
   ref=$(git symbolic-ref HEAD 2> /dev/null) || \
@@ -39,10 +41,10 @@ function parse_git_status() {
   [[ $STATUS == *\ [ADMR]* ]] && unstaged="%{$fg_bold[red]%}ﬨ";
   [[ $STATUS == *\?* ]] && untracked="%{$fg_bold[blue]%}ﬨ";
 }
-# Init timer and beam shape cursor before every cmd 
+# Init timer and beam shape cursor before every cmd
 # NOTE: preexec() and precmd() are hook functions in zsh. (bash has precmd() but not preexec())
 preexec() { echo -ne '\e[5 q'; cmd_start=$(($(print -P %D{%s%6.})/1000)) }
-precmd() { 
+precmd() {
   [ $cmd_start ] && { local cmd_end=$(($(print -P %D{%s%6.})/1000)); \
   timer="%F{222}%{$ALEX[ITALIC_ON]%}$((cmd_end-cmd_start)) ms%{$ALEX[ITALIC_OFF]%}%f" }
 }
@@ -53,6 +55,6 @@ function chpwd_prompt () {
   [[ $HPWD == *?/* ]] && { cwd_head="%F{109}%{$ALEX[ITALIC_ON]%}${HPWD%/*}/%{$ALEX[ITALIC_OFF]%}%f" \
   cwd_tail="%F{109}%B${HPWD##*/}%b%f" } || { cwd_head="%F{109}%B$HPWD%b%f"; cwd_tail="" }
 }
-# NOTE: chpwd_functions are hook funcs that will exec after pwd change  
+# NOTE: chpwd_functions are hook funcs that will exec after pwd change
 # Add to chpwd hook and trigger the chpwd hooks once, this line should appear after the prompt definition
 chpwd_functions+=(chpwd_prompt); cd .

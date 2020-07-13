a4a2734071ad939bd2d91631105d6dc21303f2e7
Alex <hellosimon1103@hotmail.com>
2020-07-13 13:10:29 +0800

    Add bootstrsp_arch script

---
+++ bootstrsp_arch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Since almost all cmds in this script need root access, you have to run it as root
+# TODO:
+pacman -S balabala
+
+
+# Using localectl to map 1. caps -> ctrl; 2. left+right shift -> caps
+# This approach is much persistent than execute setxkbmap in ~/.xinitrc
+# Settings below will not lost during bluetooth reconnection or plug/unplug keyboard
+# See: https://wiki.archlinux.org/index.php/Xorg/Keyboard_configuration
+localectl --no-convert set-x11-keymap us "" "" caps:ctrl_modifier,shift:both_capslock
+
+# Bluetooth setting
+# Privoxy config

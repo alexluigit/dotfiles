5737c8420249493cef41343b8c2bbb6365c7d331
Alex <hellosimon1103@hotmail.com>
2020-07-13 13:34:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,53 @@
-#!/bin/sh
-echo "pwd: $PWD"
-echo "Please confirm your pwd before installation."
-read -p "Press y to continue, otherwise to quit: " REPLY
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  [[ ! -d $HOME/.config ]] && mkdir ~/.config
-  echo "Creating symlink..."
-  ln -s $PWD/alacritty ~/.config/alacritty
-  ln -s $PWD/zsh/.zshenv ~/.zshenv
-  ln -s $PWD/zsh ~/.config/zsh
-  ln -s $PWD/nvim ~/.config/nvim
-  ln -s $PWD/karabiner ~/.config/karabiner
-  ln -s $PWD/tmux ~/.config/tmux
-  ln -s $PWD/tmux/tmux.conf ~/.tmux.conf
-  ln -s $PWD/vifm ~/.config/vifm
-  ln -s $PWD/scripts ~/bin/scripts
-  rm nvim/pack/bundle/opt/fzf
-  ln -s $PWD/zsh/plugins/fzf ~/.config/nvim/pack/bundle/opt/fzf
-fi
-if [[ $USER == "alex" ]]; then
-  ln -s $PWD/shadowsocks ~/.ShadowsocksX-NG
-  ln -s $PWD/ssh ~/.ssh
-  ln -s $PWD/rime ~/Library/Rime
-  ln -s $PWD/git ~/.config/git
-fi
-git submodule init && git submodule update && echo "Done."
+#!/bin/zsh
+echo "Current Directory: $PWD"
+CFGDIR="$HOME/.config"
+SCRIPTDIR="$HOME/.local/bin"
+
+check-dir(){
+  [[ ! -d $CFGDIR ]] && mkdir $CFGDIR || echo "$HOME/.config exists"
+}
+
+dotconfig() {
+  find $CFGDIR -maxdepth 1 -type l -delete
+  for i in $(du -d 1 "$PWD/config" | cut -f2); ln -s "$i" "$CFGDIR"
+  rm $CFGDIR/config
+}
+
+scripts() {
+  find $SCRIPTDIR -maxdepth 1 -type l -delete
+  for i in $(find "$PWD/local/bin" -type f -executable); ln -s $i $SCRIPTDIR
+}
+
+zsh() {
+  ln -s $PWD/zsh $CFGDIR
+}
+
+nvim() {
+  ln -s $PWD/nvim $CFGDIR
+}
+
+"$@"
+# git submodule init && git submodule update && echo "Done."
+
+# Keybinding
+# ln -s $PWD/sxhkd ~/.config/sxhkd
+# Configure shadowsocks
+# ln -s $PWD/shadowsocks ~/.config/shadowsocks
+
+# ssh config
+# ln -s $PWD/ssh ~/.ssh
+
+# Rime input
+# ln -s $PWD/rime ~/.config/ibus/rime
+
+# Git
+# ln -s $PWD/git ~/.config/git
+
+# Auto start program
+# ln -s $PWD/.xprofile ~/.xprofile
+
+# Karabiner
+# ln -s $PWD/karabiner ~/.config/karabiner
+
+# rm nvim/pack/bundle/opt/fzf
+# ln -s $PWD/zsh/plugins/fzf ~/.config/nvim/pack/bundle/opt/fzf

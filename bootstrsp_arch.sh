40c1c112c71d448afac236ec40dc20735f0f4413
Alex <hellosimon1103@hotmail.com>
2020-07-26 00:30:40 +0800

    Edit bootstrsp_arch script

--- bootstrsp_arch.sh
+++ bootstrsp_arch.sh
@@ -2,7 +2,29 @@
 # Since almost all cmds in this script need root access, you have to run it as root
 # TODO:
 pacman -S balabala
+PACKAGES=(
+  zsh
+  networkmanager
+  privoxy
+  shadowsocks-libev
 
+  xorg
+  xserver
+  xinit
+
+  rofi
+  dunst
+  bspwm
+  sxhkd
+  polybar
+  easystroke
+)
+
+BUILDS=(
+  pixom
+  xwallpaper
+  xcape
+)
 
 # Using localectl to map 1. caps -> ctrl; 2. left+right shift -> caps
 # This approach is much persistent than execute setxkbmap in ~/.xinitrc

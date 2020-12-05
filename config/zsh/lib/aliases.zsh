3b0395af498d59cf068476be7d9f4488ffe8ccab
Alex <hellosimon1103@hotmail.com>
2020-12-05 19:41:57 +0800

    feat(tmux): decouple tmux setting from project

--- config/zsh/lib/aliases.zsh
+++ config/zsh/lib/aliases.zsh
@@ -37,13 +37,15 @@ pal() { local res=$(pacman -Qeq | fzf -m --preview="pacman -Si {}"); [[ -n $res 
 
 # tmux automation
 t() {
+  local S_NAME=$(basename "${PWD//[.:]/_}")
+  local S_CONFIG=$XDG_DATA_HOME/tmux$PWD
+  [[ $1 == "edit" ]] && { mkdir -p $S_CONFIG; touch $S_CONFIG/tmux; chmod +x $S_CONFIG/tmux; nvim $S_CONFIG/tmux; return }
   # https://gist.github.com/lann/6771001
   local SOCK_SYMLINK=~/.config/ssh/ssh_auth_sock
   [ -r "$SSH_AUTH_SOCK" -a ! -L "$SSH_AUTH_SOCK" ] && ln -sf "$SSH_AUTH_SOCK" $SOCK_SYMLINK
   _switch() { tmux switch -t $1 2>/dev/null || tmux a -t $1 }
   [[ -n $@ ]] && { env SSH_AUTH_SOCK=$SOCK_SYMLINK tmux $@; return }
-  local S_NAME=$(basename "${PWD//[.:]/_}")
   HAS_SESSION=$(tmux has-session -t=$S_NAME 2>/dev/null)
-  [ $? -eq 0 ] && _switch $S_NAME || { [ -x .tmux ] && { tmux new -s $S_NAME -d && ./.tmux $S_NAME && _switch $S_NAME } \
+  [ $? -eq 0 ] && _switch $S_NAME || { [ -x $S_CONFIG/tmux ] && { tmux new -s $S_NAME -d && $S_CONFIG/tmux $S_NAME && _switch $S_NAME } \
   || env SSH_AUTH_SOCK=$SOCK_SYMLINK tmux new -s $S_NAME -d && _switch $S_NAME }
 }

fe897dcba210202295d3d233b89e75cbc30f0ea0
Alex <hellosimon1103@hotmail.com>
2021-01-02 19:02:52 +0800

    chore(misc): format, purge

--- bootstrap-draft.sh
+++ bootstrap-draft.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# Don't run this script, it's not complete yet.
 ARCH_INSTALL=(
   base
   base-devel
@@ -14,11 +13,9 @@ ARCH_INSTALL=(
 PACMAN=($(pacman -Qetq))
 YAY=($(pacman -Qmq))
 
-# Using localectl to map
-# - left+right shift -> caps
-# - ctrl_alt_bksp -> kill X session
+# Using localectl to map ctrl_alt_bksp -> kill X session
 # This approach is much persistent than execute setxkbmap in ~/.xinitrc
 # Settings below will not lost during bluetooth reconnection or plug/unplug keyboard
 # See: https://wiki.archlinux.org/index.php/Xorg/Keyboard_configuration
-localectl --no-convert set-x11-keymap us pc105 colemak caps:ctrl_modifier,shift:both_capslock_cancel,terminate:ctrl_alt_bksp
-localectl --no-convert set-keymap colemak
+# localectl --no-convert set-x11-keymap us pc105 colemak terminate:ctrl_alt_bksp
+# localectl --no-convert set-keymap colemak

ab1097f245088ddf43d206a1835edb4b7ab750af
Alex <hellosimon1103@hotmail.com>
2021-01-16 09:40:16 +0800

    chore(tmux): remove unused settings

--- config/zsh/user/functions.zsh
+++ config/zsh/user/functions.zsh
@@ -14,12 +14,9 @@ t() {
   local S_NAME=$(basename "${PWD//[.:]/_}")
   local S_CONFIG=$XDG_DATA_HOME/tmux$PWD
   [[ $1 == "edit" ]] && { mkdir -p $S_CONFIG; touch $S_CONFIG/tmux; chmod +x $S_CONFIG/tmux; nvim $S_CONFIG/tmux; return; }
-  # https://gist.github.com/lann/6771001
-  local SOCK_SYMLINK=~/.config/ssh/ssh_auth_sock
-  [ -r "$SSH_AUTH_SOCK" -a ! -L "$SSH_AUTH_SOCK" ] && ln -sf "$SSH_AUTH_SOCK" $SOCK_SYMLINK
   _switch() { [[ -n $TMUX ]] && tmux switch -t $1 2>/dev/null || tmux a -t $1; }
-  [[ -n $@ ]] && { env SSH_AUTH_SOCK=$SOCK_SYMLINK tmux $@; return; }
+  [[ -n $@ ]] && { tmux $@; return; }
   HAS_SESSION=$(tmux has-session -t=$S_NAME 2>/dev/null)
   [ $? -eq 0 ] && _switch $S_NAME || { [ -x $S_CONFIG/tmux ] && { tmux new -s $S_NAME -d && $S_CONFIG/tmux $S_NAME && _switch $S_NAME; } \
-  || env SSH_AUTH_SOCK=$SOCK_SYMLINK tmux new -s $S_NAME -d && _switch $S_NAME; }
+  || tmux new -s $S_NAME -d && _switch $S_NAME; }
 }

776ff8e49e074c245baeedd2f1c8f57c22ae4bb4
Alex <hellosimon1103@hotmail.com>
2021-03-23 03:07:48 +0800

    zsh: refactor

--- config/zsh/system/services.zsh
+++ config/zsh/system/services.zsh
@@ -1,14 +1,13 @@
-test -e $FNM_MULTISHELL_PATH || fnm use default >/dev/null &
-running() { pgrep $1 >/dev/null 2>&1; }
-running aria2c   || aria2c -i ~/.cache/aria2/aria2.session &
-running privoxy  || privoxy --no-daemon ~/.config/privoxy/config &
-running ss-local || ss-local -c ~/.config/shadowsocks/config.json &
-running mpd      || mpd &
-running emacs    || emacs --daemon &
-running udevmon  || doas nice -n -20 udevmon -c ~/.config/interception/udevmon.yaml &
+_running() { pgrep $1 >/dev/null 2>&1; }
+_startx() { exec ssh-agent startx ~/.config/x11/xinitrc -- ~/.config/x11/xserverrc vt1; }
 
-runx() {
-  if [ $(tty) = '/dev/tty1' ] && ! pgrep X; then
-    exec ssh-agent startx ~/.config/x11/xinitrc -- ~/.config/x11/xserverrc vt1
-  fi
+system_init() {
+  _running privoxy  || privoxy --no-daemon ~/.config/privoxy/config &
+  _running ss-local || ss-local -c ~/.config/shadowsocks/config.json &
+  _running mpd      || mpd &
+  _running emacs    || emacs --daemon &
+  _running udevmon  || doas nice -n -20 udevmon -c ~/.config/interception/udevmon.yaml &
+  _running aria2c   || aria2c -i ~/.cache/aria2/aria2.session >/dev/null &
+  _running X        && return
+  [[ $(tty) = '/dev/tty1' ]] && _startx || exec zsh
 }

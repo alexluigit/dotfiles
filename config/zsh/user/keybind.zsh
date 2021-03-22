3a35aba406dd450c5f6454badfc5a59911959df5
Alex <hellosimon1103@hotmail.com>
2021-03-23 03:04:23 +0800

    fixup! zsh: change keycode for f5

--- config/zsh/user/keybind.zsh
+++ config/zsh/user/keybind.zsh
@@ -1,24 +1,22 @@
 ctrl-a()  { zle beginning-of-line; }
-ctrl-b()  {}
+ctrl-b()  { zle edit-command-line; }
 ctrl-d()  { [[ -n $BUFFER ]] && zle delete-char || _fzf_cd; }
-ctrl-e()  { zle up-line-or-beginning-search; }
+ctrl-e()  { zle end-of-line; }
 ctrl-f()  { _lf; }
-ctrl-h()  { [[ -n $BUFFER ]] && zle backward-delete-char || _updir; }
 ctrl-i()  { [[ -n $BUFFER ]] && zle backward-char || fmenu 0 . "Edit: " nvim; }
-ctrl-k()  { zle edit-command-line; }
+ctrl-k()  { _fzf_kill; }
 ctrl-l()  { [[ -n $BUFFER ]] && zle vi-backward-word || zle clear-screen; }
 ctrl-n()  { zle down-line-or-beginning-search; }
 ctrl-o()  { [[ -n $BUFFER ]] && zle forward-char || fmenu; }
-ctrl-p()  { _fzf_navi z; }
+ctrl-p()  { [[ $#LBUFFER -ne $#BUFFER ]] && zle end-of-line || zle up-line-or-beginning-search; }
 ctrl-r()  { _fzf_hist; }
-ctrl-s()  {}
+ctrl-s()  { _fzf_navi z; }
 ctrl-t()  { _fzf_comp_helper; }
-ctrl-u()  { zle kill-whole-line; }
-ctrl-xx() { _fzf_kill; }
+ctrl-u()  { [[ -n $BUFFER ]] && zle kill-whole-line || _updir; }
 ctrl-y()  { _yank_cmdline; }
 ctrl-z()  { [[ -n $BUFFER ]] && zle push-input || _resume_jobs; }
-ctrl-return()  { [[ $#LBUFFER -ne $#BUFFER ]] && zle end-of-line || zle autosuggest-accept; }
-ctrl-semi()    { [[ -n $BUFFER ]] && zle vi-forward-word || _fzf_navi home; }
+ctrl-bslash()  { [[ -n $BUFFER ]] && zle vi-forward-word || _fzf_navi home; }
+ctrl-return()  { zle autosuggest-accept; }
 backspace()    { zle backward-delete-char; }
 open-angle()   { _autopairs a; }
 open-brace()   { _autopairs b; }

fcbc1a9934efcebed4ff17ddaaab00ad13baa4a7
Alex <hellosimon1103@hotmail.com>
2021-03-23 20:24:06 +0800

    awesomewm: set wallpaper

--- config/awesome/rules.lua
+++ config/awesome/rules.lua
@@ -1,5 +1,7 @@
 local ruled = require("ruled")
 local awful = require("awful")
+local gears = require("gears")
+local beautiful = require("beautiful")
 require("awful.autofocus")
 
 ruled.client.connect_signal("request::rules", function()
@@ -32,6 +34,10 @@ ruled.client.connect_signal("request::rules", function()
     callback = function(c) c:connect_signal("property::fullscreen", function() if not c.fullscreen then c.ontop = true end end) end,
     properties = {ontop = true}
   }
+end)
+
+screen.connect_signal("request::wallpaper", function(s)
+    gears.wallpaper.maximized(beautiful.wallpaper, s, false, nil)
 end)
 
 screen.connect_signal("request::desktop_decoration", function(s)
